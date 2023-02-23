2c5ace2d5f29aa634a347f2785efa3c5b1e9e003
Владимир <anfalow_w@rambler.ru>
2023-02-23 19:38:14 +0400

    Поправил опечатку

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,55 @@
 export default {
     ssr: false,
-    subdirectory: '/gb2-5/',
+    subdirectory: "/gb2-5/",
     /*
      ** Headers of the page
      */
     head: {
-        title: 'gb-курсовая',
+        title: "gb-курсовая-2-5",
         meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
             {
-                hid: 'description',
-                name: 'description',
+                hid: "description",
+                name: "description",
             },
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: 'favicon.png' },
+            { rel: "icon", type: "image/x-icon", href: "favicon.png" },
             {
-                rel: 'stylesheet',
-                href: 'https://fonts.googleapis.com/css?family=Lato:300,400,700,900',
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css?family=Lato:300,400,700,900",
             },
         ],
-        script: [{ }]
+        script: [{}],
     },
 
     router: {
-        base: '/gb2-5/',
+        base: "/gb2-5/",
     },
     /*
      ** Customize the progress-bar color
      */
-    loading: { color: '#ff4c3b', throttle: 200, height: '3px', css: true },
+    loading: { color: "#ff4c3b", throttle: 200, height: "3px", css: true },
     /*
      ** Global CSS
      */
-    css: ['@/assets/scss/app.scss', 'swiper/css/swiper.css'],
+    css: ["@/assets/scss/app.scss", "swiper/css/swiper.css"],
     /*
      ** Plugins to load before mounting the App
      */
     plugins: [
-        { src: '~/plugins/plugin.js', ssr: false },
-        { src: '~/plugins/localStorage.js', ssr: false },
-
+        { src: "~/plugins/plugin.js", ssr: false },
+        { src: "~/plugins/localStorage.js", ssr: false },
     ],
     /*
      ** Nuxt.js modules
      */
     modules: [
         // Doc: https://axios.nuxtjs.org/usage
-        'bootstrap-vue/nuxt',
-        '@nuxtjs/axios',
-        'vue-scrollto/nuxt'
+        "bootstrap-vue/nuxt",
+        "@nuxtjs/axios",
+        "vue-scrollto/nuxt",
     ],
     /*
      ** Axios module configuration
@@ -64,7 +63,7 @@ export default {
      ** Build configuration
      */
     build: {
-        transpile: ['vee-validate/dist/rules'],
+        transpile: ["vee-validate/dist/rules"],
         /*
          ** You can extend webpack config here
          */
